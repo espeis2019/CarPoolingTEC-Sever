2723566b59e068aeef9bb85cbc85669d788b8550
Crisptofer Fernández Fernández <alberto12ff@gmail.com>
2019-06-15 16:45:59 -0600

    nothing

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const route = express.Router();
 
 route.get('/', (req, res, next) => {
-    res.send("CarpoolingTEC-Server_")
+    res.send("CarpoolingTEC-Server")
 })
 
 module.exports = route;
